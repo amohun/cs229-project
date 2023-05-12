5daae63be34179535190659a6a3a862635ed9391
matt <mfoutter@stanford.edu>
2023-05-12 12:00:30 -0700

    video to image conversion

---
+++ Data/frames.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import os
+from tqdm import tqdm
+
+labels = np.loadtxt(os.path.dirname(__file__) + "/train.txt")
+
+vidcap = cv2.VideoCapture(os.path.dirname(__file__) + "/train.mp4")
+for i in tqdm(np.arange(labels.size)):
+    read, image = vidcap.read()
+    cv2.imwrite(os.path.dirname(__file__) + "/images/frame_%i.jpg" % i, image)
